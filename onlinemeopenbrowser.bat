90b4b6229bd5eea22a5a7df8986769cf4b039ff1
ItsAdamGITHUB <106012266+iheighgnfdglfgfdkhgkgfhkgfh555@users.noreply.github.com>
2024-03-15 11:43:13 +0100

    onlinemeopenbrowser.bat

--- onlinemeopenbrowser.bat
+++ onlinemeopenbrowser.bat
@@ -2,19 +2,10 @@
 scl
 echo megnyitom a böngésződ!
 start chrome.exe
-pause
 start notepad
-pause
 start notepad
-pause
-start notepad
-pause
-start notepad
-pause
 start opera.exe
-pause
 start firefox.exe
-pause
 start iexplorer.exe
 pause
 stop
